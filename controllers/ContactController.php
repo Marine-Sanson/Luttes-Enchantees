0cc61b9800e38e7d142e9e67b25568e9897cc444
marine_sanson <marine_sanson@yahoo.fr>
2022-12-14 10:45:46 +0100

    start working on contact page

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -2,11 +2,64 @@
 
 Class ContactController extends AbstractController
 {
-    public function index() :void
-    {
-        $template = "contact";
+	public function index() :void
+	{
+		$template = "contact";
 
-        $this->render($template);
+		$this->render($template);
 
-    }
+	}
+
+	public function sendMail() : void
+	{
+		if(isset($_POST["action"]) && $_POST["action"] === "sendMail")
+		{
+			$template = "contact";
+
+			if($_POST["mailAdress"] === "")
+			{
+				$errors[] = "Veuillez renseigner votre adresse mail";
+			}
+
+			if($_POST["mailObject"] === "")
+			{
+				$errors[] = "Veuillez renseigner l'objet";
+			}
+
+			if($_POST["mailContent"] === "")
+			{
+				$errors[] = "Veuillez remplir le champ contenu";
+			}
+
+			if(!isset($errors) || $errors === [])
+			{
+				$to = "marine_sanson@yahoo.fr";
+				$subject = "Luttes Enchantées - " . $_POST["mailObject"];
+				$mess = $_POST["mailContent"];
+				$message = wordwrap($mess, 70, "/r/n");
+				$headers = "De : " . $_POST["mailAdress"];
+
+				$mail = mail($to, $subject, $message, $headers);
+				var_dump($mail);
+
+				if($mail)
+				{
+					$validation = "votre mail a bien été envoyé";
+
+					$this->render($template, ["validation" => $validation]);
+				}
+				else
+				{
+					$errors[] = "il y a eu un problème, merci de recommencer";
+
+					$this->render($template, ["errors" => $errors]);
+				}
+			}
+			else
+			{
+				$this->render($template, ["errors" => $errors]);
+			}
+
+		}
+	}
 }
